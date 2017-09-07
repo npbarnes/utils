93495ab1c7528d8bd3f4c3caefad3bd8489754ce
Nathan Barnes <npbarnes@alaska.edu>
2017-09-07 12:34:45 -0800

    Fix failing tests

--- SortedSearch.py
+++ SortedSearch.py
@@ -2,56 +2,61 @@ from bisect import bisect_left, bisect_right
 import numpy as np
 
 def index(a, x, reverse=False):
-    'Locate the leftmost value exactly equal to x'
+    'Locate the leftmost (or rightmost if reversed) value exactly equal to x'
     sorter = range(len(a))
     if reverse:
         sorter = list(reversed(sorter))
 
     i = np.searchsorted(a, x, side='left', sorter=sorter)
-    if i != len(a) and a[i] == x:
-        return i
+
+    if i != len(a) and a[sorter[i]] == x:
+        return sorter[i]
     raise ValueError
 
 def find_lt(a, x, reverse=False):
-    'Find rightmost value less than x'
+    'Find largest (rightmost normally, leftmost when reversed) value less than x'
     sorter = range(len(a))
     if reverse:
         sorter = list(reversed(sorter))
 
     i = np.searchsorted(a, x, side='left', sorter=sorter)
+
     if i:
-        return i-1, a[i-1]
+        return sorter[i-1], a[sorter[i-1]]
     raise ValueError
 
 def find_le(a, x, reverse=False):
-    'Find rightmost value less than or equal to x'
+    'Find largest (rightmost normally, leftmost when reversed) value less than or equal to x'
     sorter = range(len(a))
     if reverse:
         sorter = list(reversed(sorter))
 
     i = np.searchsorted(a, x, side='right', sorter=sorter)
+
     if i:
-        return i-1, a[i-1]
+        return sorter[i-1], a[sorter[i-1]]
     raise ValueError
 
 def find_gt(a, x, reverse=False):
-    'Find leftmost value greater than x'
+    'Find least (leftmost normally, rightmost when reversed) value greater than x'
     sorter = range(len(a))
     if reverse:
         sorter = list(reversed(sorter))
 
     i = np.searchsorted(a, x, side='right', sorter=sorter)
+
     if i != len(a):
-        return i, a[i]
+        return sorter[i], a[sorter[i]]
     raise ValueError
 
 def find_ge(a, x, reverse=False):
-    'Find leftmost item greater than or equal to x'
+    'Find least (leftmost normally, rightmost when reversed) item greater than or equal to x'
     sorter = range(len(a))
     if reverse:
         sorter = list(reversed(sorter))
 
     i = np.searchsorted(a, x, side='left', sorter=sorter)
+
     if i != len(a):
-        return i, a[i]
+        return sorter[i], a[sorter[i]]
     raise ValueError
